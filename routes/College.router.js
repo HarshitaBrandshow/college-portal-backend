4e713f2c7d56845f8c49e41d4713dd49255142b1
HarshitaBrandshow <harshita.trainee@brandshow.in>
2025-02-28 10:48:46 +0530

    new chnage

--- routes/College.router.js
+++ routes/College.router.js
@@ -6,8 +6,8 @@ const { CollegeController } = require("../controller");
 
 router.post("/add-college", CollegeController.createCollege);
 router.get("/all-colleges", CollegeController.getAllColleges);
-router.get("/colleges/:id", CollegeController.getCollegeById);
-router.put("/colleges/:id", CollegeController.updateCollege);
-router.delete("/colleges/:id", CollegeController.deleteCollege);
+router.get("/:id", CollegeController.getCollegeById);
+router.put("/:id", CollegeController.updateCollege);
+router.delete("/:id", CollegeController.deleteCollege);
 
 module.exports = router;
